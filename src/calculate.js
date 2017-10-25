38ec9f85f7bac32f0a35e40b789accb6c2238a5b
jen <jen.a.freeman@googlemail.com>
2017-10-25 21:06:21 +0100

    Add object style calculate thrust and move into class

--- src/calculate.js
+++ src/calculate.js
@@ -1,64 +1,87 @@
-exports.calculatePlan=function(plan, engines) {
-    var currentMass=0;
-    var currentRockets={}
-    plan.steps.forEach(x => {
 
-        if (x.step=='add') {
-            currentMass+=x.mass;
-            if (x.rockets!=null) {
-              for (const key of Object.keys(x.rockets)) {
-                if (key in currentRockets) {
-                  currentRockets[key]+=x.rockets[key];
+
+
+
+class LeavingEarthCalculator {
+  constructor(engines) {
+    this.engines=engines;
+  }
+
+  calculatePlan(plan) {
+      var currentMass=0;
+      var currentRockets={}
+      plan.steps.forEach(x => {
+
+          if (x.step=='add') {
+              currentMass+=x.mass;
+              if (x.rockets!=null) {
+                for (const key of Object.keys(x.rockets)) {
+                  if (key in currentRockets) {
+                    currentRockets[key]+=x.rockets[key];
+                  }
+                  else {
+                    currentRockets[key]=x.rockets[key];
+                  }
+                  currentMass+=this.engines.rockets[key].weight*x.rockets[key];
                 }
-                else {
-                  currentRockets[key]=x.rockets[key];
-                }
-                currentMass+=engines.rockets[key].weight*x.rockets[key];
               }
-            }
-        } else if (x.step=='remove') {
-            currentMass-=x.mass;
-            if (x.rockets!=null) {
-              for (const key of Object.keys(x.rockets)) {
-                if (key in currentRockets) {
-                  currentRockets[key]-=x.rockets[key];
+          } else if (x.step=='remove') {
+              currentMass-=x.mass;
+              if (x.rockets!=null) {
+                for (const key of Object.keys(x.rockets)) {
+                  if (key in currentRockets) {
+                    currentRockets[key]-=x.rockets[key];
+                  }
+                  else {
+                    currentRockets[key]=-x.rockets[key];
+                  }
+                  currentMass-=this.engines.rockets[key].weight*x.rockets[key];
                 }
-                else {
-                  currentRockets[key]=-x.rockets[key];
-                }
-                currentMass-=engines.rockets[key].weight*x.rockets[key];
               }
-            }
-        }
+          }
 
-        x.currentMass=currentMass;
-        x.currentRockets=Object.assign({}, currentRockets);
-    });
+          x.currentMass=currentMass;
+          x.currentRockets=Object.assign({}, currentRockets);
+      });
 
+  }
+
+  getEngineThrustMass(selEngine, difficulty, number) {
+      var engine = this.engines.rockets[selEngine];
+      var thrust = engine.difficulty[difficulty+1]*number;
+      var mass = engine.weight*number;
+      return [thrust, mass];
+  }
+
+  calculateThrustAndMass(item, difficulty) {
+      var selEngine = item;
+      var multiplier = 1;
+      if (Array.isArray(item)) {
+          if (typeof(item[0])=="number") {
+              return this.getEngineThrustMass(item[1], difficulty, item[0])
+          } else {
+              var sum=0;
+              var mass=0;
+              item.forEach(i => {
+                  var v=this.calculateThrustAndMass(i, difficulty);
+                  sum+=v[0];
+                  mass+=v[1];
+              });
+              return [sum,mass];
+          }
+      } else if (typeof item === "string") {
+        return this.getEngineThrustMass(selEngine, difficulty, 1)
+      } else {
+          var sum=0;
+          var mass=0;
+          for (const key of Object.keys(item)) {
+             var v=this.getEngineThrustMass(key, difficulty, item[key]);
+             sum+=v[0];
+             mass+=v[1];
+          }
+          return [sum,mass];
+      }
+  }
 }
 
-function calculateThrust(item, difficulty, engines) {
-    var selEngine = item;
-    var multiplier = 1;
-    if (Array.isArray(item)) {
-        if (typeof(item[0])=="number") {
-            multiplier = item[0];
-            selEngine = item[1];
-        } else {
-            sum=0;
-            mass=0;
-            item.forEach(i => {
-                v=calculateThrust(i, difficulty, engines);
-                sum+=v[0];
-                mass+=v[1];
-            });
-            return [sum,mass];
-        }
-    }
-    var engine = engines.rockets[selEngine];
-    var thrust = engine.difficulty[difficulty+1]*multiplier;
-    var mass = engine.weight*multiplier;
-    return [thrust, mass];
-}
-
-exports.calculateThrust=calculateThrust;
+exports.LeavingEarthCalculator = LeavingEarthCalculator;
--- testsrc/test.js
+++ testsrc/test.js
@@ -7,25 +7,32 @@ var assert = require('assert');
 var plan = JSON.parse(fs.readFileSync("plan.json"));
 /* Next steps
 
-1. Move the calculator to an object to hide the rockets.
 2. Implement burn for calculatePlan
-3. Change calculateThrust to take object/dict
 */
 
 describe("Calculate", () => {
   describe("#calculateThrust", () => {
     it("Should calculate difficulty 3, a soyuz correctly with thrust of 7 and mass of 9", () => {
-        var [thrust,enginemass]= calc.calculateThrust("soyuz", 3, engines);
+        var lec = new calc.LeavingEarthCalculator(engines);
+        var [thrust,enginemass]= lec.calculateThrustAndMass("soyuz", 3);
         assert.equal(thrust, 7);
         assert.equal(enginemass, 9);
     });
     it("Should calculate difficulty 3, 3 soyuz correctly with thrust of 21 and mass of 27", () => {
-        var [thrust,enginemass] = calc.calculateThrust([3,"soyuz"], 3, engines);
+        var lec = new calc.LeavingEarthCalculator(engines);
+        var [thrust,enginemass]= lec.calculateThrustAndMass([3,"soyuz"], 3, engines);
         assert.equal(thrust, 21);
         assert.equal(enginemass, 27);
     });
     it("Should calculate difficulty 3, 4 saturns and 3 soyuz correctly with a thrust of 101 and mass of 107", () => {
-        var [thrust,enginemass] = calc.calculateThrust([[4, "saturn"], [3,"soyuz"]], 3, engines);
+        var lec = new calc.LeavingEarthCalculator(engines);
+        var [thrust,enginemass]= lec.calculateThrustAndMass([[4, "saturn"], [3,"soyuz"]], 3, engines);
+        assert.equal(thrust, 101);
+        assert.equal(enginemass, 107);
+    });
+    it("Should calculate difficulty 3, 4 saturns and 3 soyuz correctly in an object with a thrust of 101 and mass of 107", () => {
+        var lec = new calc.LeavingEarthCalculator(engines);
+        var [thrust,enginemass]= lec.calculateThrustAndMass({"saturn":4, "soyuz":3}, 3, engines);
         assert.equal(thrust, 101);
         assert.equal(enginemass, 107);
     });
@@ -34,8 +41,9 @@ describe("Calculate", () => {
   describe("#calculatePlan", () => {
     it("Should add and remove mass correctly", () => {
         var data={ "steps" : [ { "step" : "add", "mass" : 10 }, { "step" :
-        "remove", "mass": 5 } ] }
-        calc.calculatePlan(data, engines);
+        "remove", "mass": 5 } ] };
+        var lec = new calc.LeavingEarthCalculator(engines);
+        lec.calculatePlan(data);
         assert.equal(data.steps[0].currentMass, 10);
         assert.deepEqual(data.steps[0].currentRockets, []);
         assert.equal(data.steps[1].currentMass, 5);
@@ -46,7 +54,8 @@ describe("Calculate", () => {
         { "step" : "add", "mass" : 0, "rockets":{"soyuz":3,"saturn":1}},
         { "step" : "remove", "mass": 0,  "rockets": {"soyuz": 2}}
       ] };
-      calc.calculatePlan(data, engines);
+      var lec = new calc.LeavingEarthCalculator(engines);
+      lec.calculatePlan(data);
       assert.equal(data.steps[0].currentMass, 3*9+20);
       assert.deepEqual(data.steps[0].currentRockets, {"soyuz":3,"saturn":1});
       assert.equal(data.steps[1].currentMass, 9+20);
