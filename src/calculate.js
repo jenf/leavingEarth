568b33aa9d68f2067b8a833c84b49be97a31363f
Jen Freeman <jenf@users.noreply.github.com>
2017-11-01 13:24:27 +0000

    Add ion engines

--- src/calculate.js
+++ src/calculate.js
@@ -31,8 +31,15 @@ class LeavingEarthCalculator {
               if (x.mass !== undefined) {
                 currentMass-=x.mass;
               }
+              var originalMass = currentMass;
+
               if (x.rockets!== undefined) {
                 for (const key of Object.keys(x.rockets)) {
+                  if (x.step==="burn") {
+                    if (!this.engines.rockets[key].singleUse) {
+                      continue;
+                    }
+                  }
                   if (key in currentRockets) {
                     currentRockets[key]-=x.rockets[key];
                   }
@@ -47,9 +54,13 @@ class LeavingEarthCalculator {
                 }
               }
               if (x.step==='burn') {
-                var [thrust, mass] = this.calculateThrustAndMass(x.rockets, x.difficulty);
+                var time = x.time;
+                if (time == undefined) {
+                  time=1;
+                }
+                var [thrust, mass] = this.calculateThrustAndMass(x.rockets, x.difficulty, time);
                 x.totalThrust = thrust;
-                x.spareThrust = thrust-currentMass;
+                x.spareThrust = thrust-(originalMass-mass);
                 if (x.spareThrust < 0) {
                   success = false;
                   x.error = "Thrust needs to be greater than 0";
@@ -70,36 +81,45 @@ class LeavingEarthCalculator {
       return success;
   }
 
-  getEngineThrustMass(selEngine, difficulty, number) {
+  getEngineThrustMass(selEngine, difficulty, number, time=1) {
       var engine = this.engines.rockets[selEngine];
-      var thrust = engine.difficulty[difficulty-1]*number;
-      var mass = engine.weight*number;
+      var engineThrust = engine.difficulty[difficulty-1];
+      console.log("Thrust "+engineThrust);
+      if (Array.isArray(engineThrust)) {
+        engineThrust = engineThrust[time-1];
+        console.log(engineThrust);
+      }
+      var thrust = engineThrust*number;
+      var mass = 0;
+      if (engine.singleUse) {
+        mass = engine.weight*number;
+      }
       return [thrust, mass];
   }
 
-  calculateThrustAndMass(item, difficulty) {
+  calculateThrustAndMass(item, difficulty, time=1) {
       var selEngine = item;
       var multiplier = 1;
       if (Array.isArray(item)) {
           if (typeof(item[0])=="number") {
-              return this.getEngineThrustMass(item[1], difficulty, item[0])
+              return this.getEngineThrustMass(item[1], difficulty, item[0], time)
           } else {
               var sum=0;
               var mass=0;
               item.forEach(i => {
-                  var v=this.calculateThrustAndMass(i, difficulty);
+                  var v=this.calculateThrustAndMass(i, difficulty, time);
                   sum+=v[0];
                   mass+=v[1];
               });
               return [sum,mass];
           }
       } else if (typeof item === "string") {
-        return this.getEngineThrustMass(selEngine, difficulty, 1)
+        return this.getEngineThrustMass(selEngine, difficulty, 1, time)
       } else {
           var sum=0;
           var mass=0;
           for (const key of Object.keys(item)) {
-             var v=this.getEngineThrustMass(key, difficulty, item[key]);
+             var v=this.getEngineThrustMass(key, difficulty, item[key], time);
              sum+=v[0];
              mass+=v[1];
           }
--- testsrc/test.js
+++ testsrc/test.js
@@ -14,26 +14,36 @@ describe("Calculate", () => {
         assert.equal(thrust, 7);
         assert.equal(enginemass, 9);
     });
+
     it("Should calculate difficulty 5, 3 soyuz correctly with thrust of 21 and mass of 27", () => {
         var lec = new LeavingEarthCalculator(engines);
-        var [thrust,enginemass]= lec.calculateThrustAndMass([3,"soyuz"], 5, engines);
+        var [thrust,enginemass]= lec.calculateThrustAndMass([3,"soyuz"], 5);
         assert.equal(thrust, 21);
         assert.equal(enginemass, 27);
     });
+
     it("Should calculate difficulty 5, 4 saturns and 3 soyuz correctly with a thrust of 101 and mass of 107", () => {
         var lec = new LeavingEarthCalculator(engines);
-        var [thrust,enginemass]= lec.calculateThrustAndMass([[4, "saturn"], [3,"soyuz"]], 5, engines);
-        assert.equal(thrust, 101);
-        assert.equal(enginemass, 107);
-    });
-    it("Should calculate difficulty 5, 4 saturns and 3 soyuz correctly in an object with a thrust of 101 and mass of 107", () => {
-        var lec = new LeavingEarthCalculator(engines);
-        var [thrust,enginemass]= lec.calculateThrustAndMass({"saturn":4, "soyuz":3}, 5, engines);
+        var [thrust,enginemass]= lec.calculateThrustAndMass([[4, "saturn"], [3,"soyuz"]], 5);
         assert.equal(thrust, 101);
         assert.equal(enginemass, 107);
     });
 
+    it("Should calculate difficulty 5, 4 saturns and 3 soyuz correctly in an object with a thrust of 101 and mass of 107", () => {
+        var lec = new LeavingEarthCalculator(engines);
+        var [thrust,enginemass]= lec.calculateThrustAndMass({"saturn":4, "soyuz":3}, 5);
+        assert.equal(thrust, 101);
+        assert.equal(enginemass, 107);
+    });
+
+    it("Should calculate difficulty 1, 1 ion engine over 3 time with a thrust of 14 and mass of 1", () => {
+        var lec = new LeavingEarthCalculator(engines);
+        var [thrust,enginemass]= lec.calculateThrustAndMass({"ion":1}, 1, 3);
+        assert.equal(thrust, 14);
+        assert.equal(enginemass, 0); // Ion engines are not single use so cause a mass change
+    });
   });
+
   describe("#calculatePlan", () => {
     it("Should add and remove mass correctly", () => {
         var data={ "steps" : [ { "step" : "add", "mass" : 10 }, { "step" :
@@ -45,6 +55,7 @@ describe("Calculate", () => {
         assert.equal(data.steps[1].currentMass, 5);
         assert.deepEqual(data.steps[1].currentRockets, []);
     });
+
     it("Should add and remove rockets correctly and calculate mass", () => {
       var data={ "steps" : [
         { "step" : "add", "mass" : 0, "rockets":{"soyuz":3,"saturn":1}},
@@ -57,6 +68,7 @@ describe("Calculate", () => {
       assert.equal(data.steps[1].currentMass, 9+20);
       assert.deepEqual(data.steps[1].currentRockets, {"soyuz":1,"saturn":1});
     });
+
     it("Should add and calculate burns correctly and calculate mass", () => {
       var data={ "steps" : [
         { "step" : "add", "mass" : 0, "rockets":{"soyuz":3,"saturn":1}},
@@ -71,6 +83,7 @@ describe("Calculate", () => {
       assert.equal(data.steps[1].totalThrust, 35.34)
       assert.equal(data.steps[1].spareThrust, 6.340000000000003);
     });
+
     it("Burning less rockets than needed should return failure", () => {
       var data={ "steps" : [
         { "step" : "add", "mass" : 0, "rockets":{"soyuz":3,"saturn":1}},
@@ -88,6 +101,7 @@ describe("Calculate", () => {
       assert.equal(data.steps[1].spareThrust, -20.33);
       assert.notEqual(data.steps[1].error, undefined);
     });
+
     it("Removing non-existant rockets should return failure", () => {
       var data={ "steps" : [
         { "step" : "add", "mass" : 0, "rockets":{"soyuz":3,"saturn":1}},
@@ -100,6 +114,23 @@ describe("Calculate", () => {
       assert.equal(data.steps[1].currentMass, (-1*9)+20);
       assert.deepEqual(data.steps[1].currentRockets, {"soyuz":-1,"saturn":1});
       assert.notEqual(data.steps[1].error, undefined);
+    });
+
+    it("An ion thruster should lift mass with time and not remove itself", () => {
+      var data={ "steps" : [
+        { "step" : "add", "mass" : 10, "rockets":{"ion":1}},
+        { "step" : "burn", "rockets": {"ion": 1}, "time":3, "difficulty":1}
+      ] };
+      var lec = new LeavingEarthCalculator(engines);
+      assert.equal(lec.calculatePlan(data), true);
+      assert.equal(data.steps[0].currentMass, 11);
+      assert.deepEqual(data.steps[0].currentRockets, {"ion":1});
+      assert.equal(data.steps[1].currentMass, 11); // Ion rockets are not single use.
+      assert.deepEqual(data.steps[1].currentRockets, {"ion":1});
+      assert.equal(data.steps[1].totalThrust, 14)
+      assert.equal(data.steps[1].spareThrust, 3);
+      assert.equal(data.steps[1].error, undefined);
+
     });
   });
 });
